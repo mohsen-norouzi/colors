434f3a0bf220f3e90b904f396fde2a8ec76a7ffc
Mohsen <mohsen.norouzi@live.com>
2020-07-11 14:48:01 +0430

    added highlight alphabets

--- src/app/pages/color/ColorItem.js
+++ src/app/pages/color/ColorItem.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Highlighter from 'react-highlight-words';
 
 import _ from 'lodash';
 
 const ColorItem = (props) => {
-  const { color } = props;
+  const { color, bold } = props;
 
   return (
     <div className='color-item d-flex'>
       <div className='color-box' style={{ background: color }}></div>
-      <small className='color-title'>{_.startCase(color)}</small>
+      <small className='color-title'>
+        <Highlighter
+          highlightClassName='strong pr-0'
+          searchWords={[bold]}
+          highlightStyle={{ background: 'unset' }}
+          textToHighlight={color}
+        />
+      </small>
     </div>
   );
 };
--- src/app/pages/color/ColorList.js
+++ src/app/pages/color/ColorList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import ColorItem from './ColorItem';
 import { useDispatch, useSelector } from 'react-redux';
@@ -12,38 +12,77 @@ const ColorList = (props) => {
   const colors = useSelector(({ color }) => color.entities);
   const loading = useSelector(({ color }) => color.loading);
 
+  const [text, setText] = useState('');
+  const [filteredColors, setFilteredColors] = useState([]);
+
   useEffect(() => {
     dispatch(Actions.getColors());
   }, [dispatch]);
 
+  const _onTextChange = (event) => {
+    const searchKey = event.target.value;
+    setText(searchKey);
+
+    if (searchKey.trim().length) {
+      const updatedColors = colors.filter((c) => c.includes(searchKey));
+      setFilteredColors(updatedColors);
+    } else {
+      setFilteredColors([]);
+    }
+  };
+
   return (
-    <div className='colors'>
-      <FuseAnimate animation='transition.fadeIn' duration='1000' >
+    <div className='row'>
+      <FuseAnimate animation='transition.fadeIn' duration='1000'>
         <div className='w-100 text-center'>
-          <h1>Les Couleurs</h1>
+          <h1>The Colors</h1>
 
           <hr className='my-3' />
         </div>
       </FuseAnimate>
 
-      {loading && (
+      {loading ? (
         <FuseAnimate animation='transition.fadeIn' duration='1000'>
           <div className='spinner'></div>
         </FuseAnimate>
-      )}
-
-      {colors.map((color, index) => (
-        <FuseAnimate
-          key={index}
-          animation='transition.slideUpIn'
-          delay={index * 10}
-          ref={null}
-        >
-          <div className='col-sm-6 col-md-4 col-lg3 col-xl-2 p-2'>
-            <ColorItem color={color} />
+      ) : (
+        <FuseAnimate animation='transition.fadeIn' duration='1000'>
+          <div className='col-sm-6 col-md-3 col-lg-3 col-xl-3 mx-auto mb-5'>
+            <input
+              className='form-control text-center'
+              type='text'
+              placeholder='search'
+              onChange={_onTextChange}
+              value={text}
+            />
           </div>
         </FuseAnimate>
-      ))}
+      )}
+
+      <div className='colors'>
+        {filteredColors.length
+          ? filteredColors.map((color, index) => (
+              <FuseAnimate
+                key={index}
+                animation='transition.slideUpIn'
+                delay={index * 10}
+              >
+                <div className='col-sm-6 col-md-4 col-lg-3 col-xl-2 p-2'>
+                  <ColorItem color={color} bold={text} />
+                </div>
+              </FuseAnimate>
+            ))
+          : !loading &&
+            (text.trim() != '' ? (
+              <FuseAnimate animation='transition.fadeIn'>
+                <p>no colors found</p>
+              </FuseAnimate>
+            ) : (
+              <FuseAnimate animation='transition.fadeIn'>
+                <p>type to search colors</p>
+              </FuseAnimate>
+            ))}
+      </div>
     </div>
   );
 };
